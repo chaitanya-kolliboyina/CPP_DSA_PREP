b5f8876180ed5941fc66db7b39da21ebf1c5027f
Chaitanya-Endurance <kvschaitanya17@gmail.com>
2023-11-13 21:43:11 +0530

    Reverse of number

---
+++ Basci math/Reverse_number.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int reverse(int n){
+    int rev = 0;
+    while(n>0){
+        int rem = n%10;                          // Time complexity : O(n)
+        rev = rev*10 + rem;                      // Space complexity : O(1)
+        n = n/10;
+    }
+    return rev;
+}
+int main(){
+    int x;
+    cin>>x;
+    cout<<reverse(x);
+    return 0;
+}
